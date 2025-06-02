dff1e2723ac276c28dac725e2a09cff348fde646
Nikita Nemkov <nkreamsov@gmail.com>
2025-06-02 11:45:57 +0300

    comp hw10

---
+++ src/js/page-3.js
@@ -0,0 +1,38 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const delayInput = form.elements.delay;
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const delay = Number(delayInput.value);
+  const state = form.elements.state.value;
+
+  createPromise(delay, state)
+    .then(delay => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
